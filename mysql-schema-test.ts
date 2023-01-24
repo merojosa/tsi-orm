f2d616ec09b30a6fdbf1af7561eb66445224f446
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-01-24 02:21:51 -0600

    Map table name (kinda ugly tbh)

--- mysql-schema-test.ts
+++ mysql-schema-test.ts
@@ -6,7 +6,7 @@
 
 type MySqlDataTypes = "int" | "varchar" | "date";
 
-type MySqlColumn =
+type MySqlColumn<TableNames> =
   | {
       type: MySqlDataTypes;
       length: number;
@@ -16,15 +16,18 @@ type MySqlColumn =
     }
   | {
       type: "relation";
-      relatedTable: string; // TODO: Existing tables that the user entered
+      relatedTable: TableNames;
       relatedColumns: string[]; // TODO: The columns for the chosen table
     };
 
-type MySqlEntity = Record<string, MySqlColumn>;
+type MySqlEntity<TableNames> = Record<string, MySqlColumn<TableNames>>;
 
-type MySqlSchema = Record<string, MySqlEntity>;
+type MySqlSchema<TableNames extends string> = Record<
+  TableNames,
+  MySqlEntity<TableNames>
+>;
 
-export const schema: MySqlSchema = {
+export const schema: MySqlSchema<"User" | "Post"> = {
   User: {
     email: { type: "int", length: 10 },
     password: { type: "date", length: 20 },
